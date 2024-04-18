b50604f3f9117700cd44207762f3ecc8a8616d9b
Caro <15948931+CarosCave@users.noreply.github.com>
2024-04-18 10:50:31 +0200

    Update MainWindowViewModel and MainWindow.axaml
    The MainWindowViewModel class has been updated to improve state-tracking related to log entries. A private list, "_bands", was introduced to replace "_band", while "AnzahlBeiträge" now accurately reflects the count of Logbuch entries for output. Changes were also made to the MainWindow.axaml file to include a label displaying the number of contributions.

--- Logbuch/ViewModels/MainWindowViewModel.cs
+++ Logbuch/ViewModels/MainWindowViewModel.cs
@@ -10,36 +10,51 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty] private string _rufzeichen = "DK1CMB";
 
     [ObservableProperty] private string _name = "Caroline Claußen";
+    
+    [ObservableProperty] private string _band = "20 m";
+    
+    // public List<string> Bands { get; }
 
-    //[ObservableProperty] private string _band;
-    
-    //public string? Name = "Caroline Claußen";
-    //private string? _band = "20 m";
-    private string _band = "20 m";
-    
-    public string Band 
+    [ObservableProperty] private List<string> _bands = new List<string>()
     {
-        get => _band;
-        set
-        {
-            OnPropertyChanged(nameof(Band));
-        }
-    }
-    public List<string> Bands { get; }
+        "80 m",
+        "40 m",
+        "20 m",
+        "2 m",
+        "70 cm"
+    };
     
     public LogbuchEintrag Eintrag = new LogbuchEintrag();
     private List<LogbuchEintrag> Logbuch = new List<LogbuchEintrag>();
 
+    
+    private int _anzahlEinträge;
+
+    public int AnzahlBeiträge
+    {
+        get
+        {
+            return _anzahlEinträge;
+        }
+        set
+        {
+            if (_anzahlEinträge != Logbuch.Count)
+            {
+                _anzahlEinträge = Logbuch.Count;
+                OnPropertyChanged(nameof(AnzahlBeiträge));
+            }
+        }
+    }
+    
     public void TriggerNewEntry()
     {
-        
-
         Eintrag.CallSign = Rufzeichen;
         Eintrag.OperatorName = Name;
         Eintrag.Band = Band;
         
         Logbuch.Add(Eintrag);
         Eintrag = new LogbuchEintrag();
+        
     }
 
     public void TriggerConsoleEntry()
@@ -55,7 +70,7 @@ public partial class MainWindowViewModel : ViewModelBase
     
     public MainWindowViewModel()
     {
-        Bands = new List<string> { "80 m", "40 m", "20 m", "2 m", "70 cm"};
+        
     }
     
 }
